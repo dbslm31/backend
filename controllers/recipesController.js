608641d782cdc5e29d89b3fa9a5680165d4dfead
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2023-06-19 09:50:29 +0200

    ChangedAPI-getRecipes_getRecipesById
    Changed the API and  updated getRecipes and getRecipeById functions

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -5,25 +5,43 @@ const apiUrl = "https://api.api-ninjas.com/v1/recipe";
 
 //Récupérer toutes les recettes
 exports.getRecipes = async (req, res) => {
+  const axios = require("axios");
+
+  const { query } = req.body;
+
+  const options = {
+    method: "GET",
+    url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?${query}`,
+    headers: {
+      "X-RapidAPI-Key": "dee7f09561mshc6dcbfc0ec71aadp179958jsn32a3b9d31700",
+      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    },
+  };
+
   try {
-    const query = req.body.query;
-
-    const response = await axios.get(apiUrl, {
-      params: {
-        query: query,
-      },
-      headers: {
-        "X-Api-Key": apiKey,
-      },
-    });
-    res.status(200).json("Voici le résultat de la recherche");
-
-    const recipes = response.data;
-    console.log(recipes);
+    const response = await axios.request(options);
+    res.send(response.data);
   } catch (error) {
-    console.error(
-      "Oups il y a eu une erreur pendant la récupération des recettes depuis l'API",
-      error
-    );
+    console.error(error);
+  }
+};
+
+// Afficher les informations d'une recette
+exports.getRecipeById = async (req, res) => {
+  const { id } = req.params;
+  const options = {
+    method: "GET",
+    url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+    headers: {
+      "X-RapidAPI-Key": "dee7f09561mshc6dcbfc0ec71aadp179958jsn32a3b9d31700",
+      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
   }
 };
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const recipesController = require("../controllers/recipesController");
 
-router.get("/all", recipesController.getRecipes);
+router.get(`/all`, recipesController.getRecipes);
+router.get(`/:id`, recipesController.getRecipeById);
 
 module.exports = router;
