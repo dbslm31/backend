47ac7b6defef111e33111e3c5094e2e4b4c5977c
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2023-06-19 10:46:04 +0200

    deleteRecipe - updateRecipeOrder
    Added functions and routes to allow user to delete a recipe or to change order of the recipe in the list

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,7 +1,5 @@
 const axios = require("axios");
-
-const apiKey = process.env.API_KEY;
-const apiUrl = "https://api.api-ninjas.com/v1/recipe";
+const pool = require("../db");
 
 //Récupérer toutes les recettes
 exports.getRecipes = async (req, res) => {
@@ -43,5 +41,73 @@ exports.getRecipeById = async (req, res) => {
     res.send(response.data);
   } catch (error) {
     console.error(error);
+  }
+};
+
+//Ajouter une recette au menu
+exports.addRecipeToMenu = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const url = req.url;
+
+    const recipeId = id.split("/").pop();
+
+    const newRecipe = await pool.query(
+      "INSERT INTO menu (recipe_id, recipe_url) VALUES ($1, $2)",
+      [parseInt(recipeId), url]
+    );
+    console.log("La recette à bien été ajoutée");
+    res.json(newRecipe.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .send(
+        "Oups, il y a eu une erreur pendant l'ajout de la recette au menu'"
+      );
+  }
+};
+
+//Supprimer une recette du menu
+exports.deleteRecipeFromMenu = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedRecipe = await pool.query(
+      "DELETE FROM menu WHERE recipe_id = $1",
+      [id]
+    );
+    console.log("La recette a bien été supprimée du menu");
+    res.json(deletedRecipe.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .send(
+        "Oups, une erreur s'est produite lors de la suppression de la recette du menu."
+      );
+  }
+};
+
+//Modifier l'ordre de la recette dans le menu
+// Modifier l'ordre de la recette dans le menu
+exports.updateRecipeOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { newOrder } = req.body;
+
+    const updatedRecipe = await pool.query(
+      "UPDATE menu SET id = $1 WHERE recipe_id = $2",
+      [newOrder, id]
+    );
+    console.log("L'ordre de la recette a été modifié dans le menu");
+    res.json(updatedRecipe.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .send(
+        "Oups, une erreur s'est produite lors de la modification de l'ordre de la recette dans le menu."
+      );
   }
 };
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -4,5 +4,8 @@ const recipesController = require("../controllers/recipesController");
 
 router.get(`/all`, recipesController.getRecipes);
 router.get(`/:id`, recipesController.getRecipeById);
+router.post(`/:id`, recipesController.addRecipeToMenu);
+router.delete(`/:id`, recipesController.deleteRecipeFromMenu);
+router.put(`/:id`, recipesController.updateRecipeOrder);
 
 module.exports = router;
